fe8b683e630e5e394def836f3edadb97268de180
zqk <645360569@qq.com>
2025-11-05 16:30:49 +0800

    【MOD】 简化登录

--- login/login_service.py
+++ login/login_service.py
@@ -32,44 +32,7 @@ class LoginService:
         )
         return self.authenticator
 
-    def get_authenticator_with_email_support(self):
-        """
-        获取支持邮箱登录的认证器实例
-        通过创建一个新的credentials字典，使邮箱可以作为用户名登录
-        """
-        if self.config is None:
-            self._load_config()
 
-        # 创建支持邮箱登录的credentials副本
-        email_supported_credentials = {'usernames': {}}
-        
-        # 复制原始用户名登录信息
-        for username, user_info in self.config['credentials']['usernames'].items():
-            email_supported_credentials['usernames'][username] = user_info.copy()
-            
-        # 添加邮箱作为额外的用户名入口
-        for username, user_info in self.config['credentials']['usernames'].items():
-            if 'email' in user_info:
-                email = user_info['email']
-                # 确保邮箱不是None或空字符串
-                if email and email not in email_supported_credentials['usernames']:
-                    # 使用邮箱作为用户名的副本，保持密码和其他信息相同
-                    email_supported_credentials['usernames'][email] = user_info.copy()
 
-        # 创建支持邮箱登录的认证器
-        self.authenticator = stauth.Authenticate(
-            email_supported_credentials,
-            self.config['cookie']['name'],
-            self.config['cookie']['key'],
-            self.config['cookie']['expiry_days']
-        )
-        return self.authenticator
 
-    def save_cookie(self):
-        """
-        保存cookie配置
-        """
-        with open('./login/config.yaml', 'w', encoding='utf-8') as file:
-            yaml.dump(self.config, file, default_flow_style=False, allow_unicode=True)
-        return self.config['cookie']
 
--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from yaml.loader import SafeLoader
 import datetime
 import login.login_service as lsogin_service
 
+login_service = lsogin_service.LoginService()
 # 加载配置，使用支持邮箱登录的认证器
-authenticator = lsogin_service.LoginService().get_authenticator_with_email_support()   
+authenticator = login_service.get_authenticator()   
 
 try:
     authenticator.login()
-
     if st.session_state.get('authentication_status'):
         st.success("登录成功")
         authenticator.logout()
